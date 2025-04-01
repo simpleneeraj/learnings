a7d924952ee54bd814ada9333fd521f3b48fbc49
“simpleneeraj” <simplneeraj@gmail.com>
2025-04-02 00:07:58 +0530

    Css Changes

---
+++ notes.md
@@ -0,0 +1,5 @@
+step 1 - login/page.tsx
+step 2 - /login
+
+React - Next [Rich in Features]
+Javascript - Typescript [Rich in Features]
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,10 @@
 const LoginPage = () => {
-  return 'Hii Neeharika';
+  return (
+    <div className="login-page">
+      <div>
+        <h1>Hi Neeharika Medam</h1>
+      </div>
+    </div>
+  );
 };
 export default LoginPage;
